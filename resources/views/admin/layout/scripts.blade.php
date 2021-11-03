a068b1ac4ee5c104c384b66d63bcc92fe913dfcc
Goldie313 <dcruzgoldie5@gmail.com>
2021-11-03 16:27:06 +0530

    goldie commit

--- resources/views/admin/layout/scripts.blade.php
+++ resources/views/admin/layout/scripts.blade.php
@@ -396,12 +396,12 @@ var chart1 = new CanvasJS.Chart("chartContainer1", {
                 {
                     type: "doughnut",
                     startAngle: 60,
-                    //innerRadius: 60,
+                    innerRadius: 90,
                     indexLabelFontSize: 17,
-                    indexLabel: "{label} - #percent%",
-                    toolTipContent: "<b>{label}:</b> {y} (#percent%)",
+                    //indexLabel: "{label} - #percent%",
+                    //toolTipContent: "<b>{label}:</b> {y} (#percent%)",
                     dataPoints: [
-                        { y: 67, label: "s" },
+                        { y: 67, label: "Other" },
                         { y: 28, label: "Archives" },
                         { y: 10, label: "Labels" },
                         { y: 7, label: "Drafts" },
